8a89e85a94b2d36ad1c4d6dca593ae8dacb73c60
banshikha <banshikhabn12@gmail.com>
2024-12-23 10:33:35 +0000

    datatype

---
+++ basic01/test2.js
@@ -0,0 +1,17 @@
+"use strict";
+alert(hello);
+/* prefer not to use var bcz of issue in block scope and functional scope */
+/* alert() can not be used as we r using node js not browser*/
+//null datatype is an object where as undefined datatype is undefined itself
+//data types
+/* 
+no
+bigint
+string
+boolean
+null
+undefined
+symbol
+object 
+*/
+//gives the data type console.log(type of "")
